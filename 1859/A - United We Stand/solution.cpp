268f1609d479b26537fa440f4ba1bbdff97a5636
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:35:40 +0700

    Add United We Stand [A] from Codeforces

---
+++ 1859/A - United We Stand/solution.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define EYMK signed main()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fi first
+#define se second
+#define each(x) for(auto & i : x)
+#define vvi vector<vi>
+#define pb push_back
+#define endl '
+'
+#define yesno(x) cout << ((x) ? "YES
+" : "NO
+")
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+ 
+const int MOD = 1e9 + 7;
+const int LARGEPRIME = 998244353;
+const int INF_INT = 2147483647;
+const ll INF_LL = 2 * 1e18;
+const int dx[] = {-1, 1, 0, 0};
+#define start_time auto start = clock();
+#define cerr_time cerr <<endl<< "Time: " << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms
+";
+const int dy[] = {0, 0, -1, 1};
+const int dx8[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 0, -1, 1, -1, 1, -1, 1};
+template<typename T, typename T1>T amax(T &a, T1 b){if(b>a)a=b;return a;}
+template<typename T, typename T1>T amin(T &a, T1 b){if(b<a)a=b;return a;}
+ 
+void solve()
+{
+    int n; cin>>n;
+    int a[n + 1];
+    set<int> ini_check;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+        ini_check.insert(a[i]);
+    }
+    sort(a, a + n);
+    if(ini_check.size() == 1)
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    int idx = 0;
+    while(a[idx] == a[0])idx++;
+    cout<<idx<<" "<<n-idx<<endl;
+    for(int i = 0; i < idx; i++) cout<<a[i]<<" ";
+    cout<<endl;
+    for(int i = idx; i < n; i++)cout<<a[i]<<" ";
+    cout<<endl;
+ 
+}
+ 
+EYMK
+{
+    fast;
+ 
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+    return 0;
+}
