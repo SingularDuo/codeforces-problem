4c6b30f7eff94c66297504cdec62d047917f34ff
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:57:26 +0700

    Add Jzzhu and Numbers [D] from Codeforces

---
+++ 449/D - Jzzhu and Numbers/solution.cpp
@@ -0,0 +1,150 @@
+/*
+⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢻⣿⣿⣿⡿⠙⠉⣉⡉⠉⠉⠉⠉⠉⠉⣉⡉⠉⠛⢯⣍⠉⠉⠉⠙⢟⡋⢉⣽⣿⣿⣏⠉⠉⠉⠉⢉⣉⣉⣉⣉⣉⡉⠭⠭⠭⠭
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⡄⠀⠀⠀⠀⠀⢸⡼⠟⠁⠀⣠⣾⡿⠀⢀⣤⡀⠀⠀⢶⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⡿⠃⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⠀⠀⠠⢤⣀⠀⣶⢹⢀⡇⠀⠀⠀⠀⣠⠞⠀⠀⠀⠘⠿⠋⠀⠀⠋⠀⠉⠀⠀⠀⠈⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⣀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠢⢄⡀⠈⠉⢙⣾⠮⠍⠙⠓⠲⣦⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀
+⡀⢐⠐⣲⣤⣀⠉⠓⠂⡞⠀⡰⠚⠙⠓⠲⢼⡦⣀⠀⠀⠈⠛⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠘⣇⠀⠀⣈⣀⠀⢀⣔⣶⡖⠒
+⠁⠀⡄⠀⠉⠛⠿⣶⢰⠃⣰⠁⠀⠀⣀⡴⢋⣥⠿⢓⣲⣾⠿⢍⣉⠐⣻⡤⢠⡀⠓⠦⣄⣀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠂⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⢹⢖⣋⠤⠼⠯⡻⣿⡖⠖⢻
+⠭⠥⠷⠶⡆⣀⡀⠀⡞⢠⠇⠀⢠⡾⣋⡔⠉⠀⣠⡾⠋⠀⠀⣠⣾⡫⠕⠻⣼⠹⡤⢀⣀⠈⠉⢯⠓⠒⠒⠲⣾⣳⡶⠶⠒⠲⣄⣀⣤⠞⠶⣄⡀⣴⡿⠋⠀⠀⠀⠀⠙⡞⣧⡀⠸
+⠍⠋⠛⣄⣳⠈⠙⢳⡇⡸⠀⣠⢎⢴⠏⠀⠀⣼⠋⠀⠀⢀⣼⠟⠁⠀⠀⠀⠹⣧⢱⡀⠀⠉⠁⠘⢳⣄⠀⠉⠈⢿⡌⠉⠒⠢⡨⣳⡍⠑⠦⡈⢿⡋⠀⢀⡠⠴⠒⢦⡀⠸⣽⡩⠭
+⠶⠾⠿⠟⠫⣤⣀⣼⡇⡇⣰⣣⢫⠃⠀⢠⡾⠁⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠀⠘⢇⠱⡄⠀⠀⠀⠀⠉⢆⠀⠀⠈⢷⠀⠀⠀⠹⡜⢽⡗⠦⡈⠪⣳⣔⠋⠀⣀⣴⠚⠃⡄⢻⣇⠀
+⣉⣈⣨⣷⡄⠀⠉⣻⡿⡽⡱⢁⠇⠀⢠⡟⠁⠀⢀⡜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡙⢆⠀⠀⠀⠀⠈⢆⠀⠀⠘⣇⠀⠀⠀⢣⠈⢿⡄⠈⠢⡈⡙⢦⡖⠁⠀⠹⡄⠱⡘⣿⠤
+⠶⢆⡲⣿⣦⠾⠷⢾⣷⡳⠁⡎⠀⢀⡿⠁⠀⣀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢮⡳⠀⡀⠀⠀⠈⢆⠀⠀⢨⡄⠀⠀⠸⠀⠘⣧⡤⠤⠛⢇⡎⣇⠀⢀⠀⡇⠀⢣⢿⡍
+⣄⣈⠇⣻⡟⣾⠀⡼⡱⠁⡸⠀⠀⣼⠃⠀⡴⠃⢀⡤⠄⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠚⠓⠛⢦⡙⠳⡄⠀⠈⢆⠀⠀⡆⠀⠀⠀⠀⠀⠸⡄⢀⡤⠞⡁⡿⡄⢸⠀⡇⠀⠘⡜⡇
+⠓⠒⠀⠉⠛⢿⢺⡳⠁⢠⡇⠀⢠⡇⢀⡜⠡⣞⣁⣀⣀⡸⠇⠀⠀⠀⠀⠀⠀⠀⠈⠑⠦⠤⠤⠤⠟⢦⣀⠀⠀⠈⢆⠀⡇⠀⠀⠀⠀⠀⢠⢻⠉⠀⢠⢣⣿⣷⣸⠀⡇⠀⠀⡇⢘
+⡄⠤⠴⠶⠖⣺⢷⢃⠀⣸⠀⠀⣸⢳⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠙⠳⣤⡀⠘⣦⡇⠀⠀⢸⠀⠀⢸⡼⡶⠔⢁⣾⣻⣿⡿⢸⠁⠀⠀⠀⢸
+⠀⠠⠤⠤⠴⣟⡏⡎⠀⡏⠀⠀⡽⡏⠀⠀⠀⣀⣤⠤⠖⠚⡃⠀⠀⠀⠀⠀⠀⠀⠀⢈⡛⠚⠳⠤⣄⡀⠀⠀⠈⠛⠯⣷⠇⠀⠀⡞⠀⠀⢸⡇⣿⢠⡾⢃⡇⣿⡇⡼⠀⠀⠀⡀⢸
+⠀⠀⠀⠀⢰⢹⢠⠁⠀⢷⠀⠀⣿⢻⢀⡀⣀⣡⣤⣶⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⣽⣿⣶⣦⣄⣀⠀⠀⠀⠀⠀⡟⠀⠀⢠⠇⠀⠀⢸⠃⢸⠋⢠⠞⣼⡟⢱⠇⠀⠀⠀⡇⢸
+⡲⣄⡀⠀⡟⡇⠀⠀⠀⢸⡂⢰⢸⡞⡟⠛⣿⣿⡿⠿⠛⠉⠁⠀⠲⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⢿⣿⣿⣿⣷⣦⡄⢠⠀⠀⠀⠈⠀⠀⠀⡎⠀⢸⡒⠚⠚⠛⠓⠛⠶⠦⢤⣈⣁⢸
+⠉⠳⢭⡳⡇⡇⢠⠀⠀⠀⣧⠈⣧⢻⡇⢀⠌⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠃⠀⣠⠃⠀⠀⢀⡀⠀⠀⡸⠁⡄⢸⠣⠤⠖⠒⠒⠒⠦⠤⡄⠀⠉⠙
+⠀⠀⠰⢯⣇⣧⢸⡄⢠⠀⠈⢦⡈⢿⣄⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠃⣰⠀⢀⠞⢠⠀⡴⠃⣸⠀⢸⠀⠀⠀⠀⠀⢀⡠⠊⠁⠀⠀⠉
+⠀⠀⣠⢞⣿⣜⣤⢷⣸⠳⣄⠈⠻⣖⠙⠛⠃⠀⠀⠀⠀⠀⠀⠀⢠⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣞⡴⣺⢃⣴⡏⣠⢏⡴⣡⢶⠇⠀⡟⠛⠓⠆⠐⠒⠁⠀⠀⠀⣀⣠⠴
+⠒⠞⣟⣛⠓⠚⠿⠬⣿⡇⠈⠙⡖⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣑⣋⣾⠾⠷⠾⣏⣱⣏⢀⣼⣁⡀⠀⣴⠒⣲⡤⣴⠒⠋⠁⠀⠀
+⡛⠵⢖⣦⢭⣑⠢⠤⣀⡈⠙⠒⠾⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⠤⠔⣒⣛⡭⠉⢁⣀⡠⠤⠤⠐⢒⣒⣋⠭⠭⣿⣗⣻⣭⣭⣿⡽⡄⡇⠀⠀⠀⠀⠀
+⡝⠛⠶⣮⣭⣓⡫⢕⣲⠭⡑⢢⣤⣀⠉⠛⠷⣄⣀⣀⣤⣀⣀⣀⣀⣀⣀⣀⡤⠖⣛⡩⠴⣒⡪⠭⠔⢒⣋⡉⠥⣤⣒⣲⡭⢽⣗⣒⣾⡯⢽⣿⠛⠿⡛⢿⣿⡏⣿⡇⠀⠀⠀⢠⠀
+⢤⢌⣀⢀⣜⠙⡻⡿⢾⣭⣟⡲⠭⣟⠟⡂⠀⢮⡙⢦⣠⡇⣯⣿⣉⡿⢋⠥⠖⡩⠔⡂⠭⣔⣒⡮⣽⣗⣲⡿⢽⡿⠲⢟⠙⠛⡏⣠⣀⢌⠉⢀⡀⠀⢉⢸⣿⣭⡽⠃⠀⠀⠀⡞⠀
+⡇⢀⠀⠈⠀⠀⠀⠀⢥⡂⠉⠛⠿⣷⣶⣍⣽⣂⣷⠤⢥⣤⠴⠶⣓⣤⣭⣭⣖⣻⣭⣭⠿⠷⠛⡋⢋⠅⠉⠑⠖⠉⠛⠀⠀⠀⠈⠉⠀⠀⠑⠁⠀⡀⢠⣞⡟⠛⢷⣄⠀⠀⣰⠃⠀
+⣅⠀⠁⢀⠠⠀⠀⠀⠀⠀⠢⡀⠀⢹⣿⣿⣛⠿⠿⢯⣭⣭⣿⠿⠿⠻⣿⣿⡟⣩⠁⠀⠀⠐⣈⢄⣶⢠⡄⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡧⣄⣈⠻⣷⣤⡟⠀⠀
+⡇⢀⠀⠀⢅⢀⠀⠐⠀⠀⠀⠓⠀⠤⠋⢹⡿⠃⡉⠉⠀⠁⠈⣧⡐⠀⢿⣿⡇⠅⠠⠆⠞⠉⠁⠀⠘⢂⠁⠀⢐⠀⡡⢄⠀⣄⣀⠀⠀⠀⠠⠀⠀⢠⡇⣿⣏⠙⠚⠿⣾⣿⣧⡀⠀
+⡇⠀⠀⠄⣨⣆⠢⠀⢀⠀⠀⠀⠀⠀⢄⣸⡗⡲⠤⠁⣀⠀⠄⠠⠀⡀⢰⠿⣇⠀⠀⡆⠀⠠⣄⣀⠀⠀⠈⠆⠰⡁⠊⢀⣄⢱⠆⠰⠀⠈⠁⠠⣶⡆⣧⣿⣏⡉⠒⢤⣄⡙⢛⣿⣤
+⢶⣄⠀⠀⠀⠈⠀⢧⡀⠒⡐⠄⠠⡆⡟⣿⡇⠃⠀⠀⠄⠀⠀⠀⠀⠈⢸⠰⣿⣦⡄⠃⠀⠀⠀⠀⡀⠀⠀⢁⠂⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⣿⣿⣟⡉⠘⠳⢾⡻⢶⣍⣬
+⠀⣽⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠳⣧⣾⡆⠀⠏⠀⠀⡀⠀⢀⠀⠀⢸⣤⣿⡇⠀⠀⠀⠀⠃⠈⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⢋⣄⠀⠀⠙⣶⡄⠈⣟⢆⢻⣿
+⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢸⣿⣇⢄⠀⠘⠦⣀⠀⠀⠀⠀⠈⣿⢻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣕⠋⠒⣉⣀⠀⡿⣷⠀⢹⠘⡆⣯
+⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⣿⣿⣘⡖⠶⠒⠶⠦⠔⠂⣶⠰⣿⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢽⣦⡬⢤⣒⣪⣇⣇⣴⣻⠦⠿⣾
+⣿⣿⣷⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣇⣝⣿⣇⣷⣀⣀⣐⣈⣂⣁⣸⣀⣿⣍⣀⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣾⣛⣛⣉⣩⣭⣥⣶⣒⣒⣚⣋⣲⣶⣐
+Author: duxp
+"The king has returned, and the throne has been reclaimed.",
+"The racer has returned, and the track is ready to be fired up again.",
+"The coder has returned, and new problems are ready to be solved.",
+Date: 2026-08-11 09:32:50
+Problem name: D. Jzzhu and Numbers
+Problem URL: https://codeforces.com/problemset/problem/449/D
+Idea: 
+Tag used: 
+Summary: 
+*/
+#include <cassert> 
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <iterator>
+#include <chrono>
+using namespace std;
+#define int long long
+#define ll long long
+#define ld long double 
+#define pb push_back
+#define fi first
+#define se second
+#define endl '
+'
+#define all(x) x.begin() + 1, x.end()
+#define nall(x) x.begin(), x.end()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, x) memset(a, x, sizeof(a))
+#define YESNO(x) cout << ((x) ? "YES" : "NO")
+#define KILL() exit(0)
+#define NAME "duxp"
+#define _______TOISECOGIAIVOI_______ signed main()
+using vi = vector<int>; using pii = pair<int, int>; using vpii = vector<pii>;
+template<typename T, typename = void> struct is_it : std::false_type {}; template<typename T> struct is_it<T, typename std::conditional<true, void, decltype(std::begin(std::declval<T>()))>::type> : std::true_type {};
+void _db(long long x) { std::cerr << x; } void _db(double x) { std::cerr << x; } void _db(char x) { std::cerr << '\'' << x << '\''; } void _db(const std::string &x) { std::cerr << '"' << x << '"'; } void _db(bool x) { std::cerr << (x ? "true" : "false"); } template<typename A, typename B> void _db(const std::pair<A, B> &p) { std::cerr << "("; _db(p.first); std::cerr << ", "; _db(p.second); std::cerr << ")"; } template<typename T, typename std::enable_if<is_it<T>::value && !std::is_same<T, std::string>::value, int>::type = 0> void _db(const T &v) { std::cerr << "["; bool f = 1; for (const auto &x : v) { if (!f) std::cerr << ", "; _db(x); f = 0; } std::cerr << "]"; }
+void _pr() { std::cerr << "
+"; } template <typename T, typename... V> void _pr(T t, V... v) { _db(t); if (sizeof...(v)) std::cerr << ", "; _pr(v...); } 
+#ifndef ONLINE_JUDGE
+#define debug(x...) std::cerr << "[" << #x << "] = ", _pr(x)
+#else
+#define debug(x...)
+#endif
+template <typename T> constexpr int getbit(T x, int k) { return (x >> k) & 1; }
+template <typename T> constexpr T onbit(T x, int k) { return x | (T(1) << k); }
+template <typename T> constexpr T offbit(T x, int k) { return x & ~(T(1) << k); }
+constexpr int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, 1, -1};
+constexpr long long OO = 0x7FFFFFFFFFFFFFFFLL;
+constexpr int oo = 0x7FFFFFFF, INF = 0x3f3f3f3f, LLINF = 0x3f3f3f3f3f3f3f3f;
+constexpr int MOD = 1000000007, MOD2 = 998244353, MAXN = 2000000, LOG = 20, BLOCK = 450;
+constexpr int ALPHA = 26, BIT = 20;
+const unsigned long long NEXT_PRIME[] = {0, 11ULL, 101ULL, 1009ULL, 10007ULL, 100003ULL, 1000003ULL, 10000019ULL, 100000007ULL, 1000000007ULL, 10000000019ULL};
+ 
+template <class T, class U> inline bool minimize(T &x, const U &y) { return x > y ? (x = y, true) : false; }
+template <class T, class U> inline bool maximize(T &x, const U &y) { return x < y ? (x = y, true) : false; }
+ 
+int n;
+int f[MAXN + 5];
+int p2[MAXN + 5];
+ 
+_______TOISECOGIAIVOI_______
+{
+    fast;
+    if (fopen(NAME ".inp", "r")) {
+        freopen(NAME ".inp", "r", stdin);
+        freopen(NAME ".out", "w", stdout);
+    }
+    cin>>n;
+    p2[0] = 1;
+    for(int i = 1; i <= n; i++) p2[i] = (p2[i - 1] % MOD * 2) % MOD;
+    for(int i = 0; i < n; i++)
+    {
+        int x ; cin>>x;
+        f[x]++;
+    }
+    int maxMask = 1 << BIT; 
+    for (int i = 0; i < BIT; i++) for (int mask = maxMask - 1; mask >= 0; mask--) if (!getbit(mask, i)) 
+    {
+        f[mask] = (f[mask] + f[onbit(mask, i)]) % MOD;
+    }
+    int res = 0;
+    for(int mask = 0; mask < maxMask; mask++)
+    {
+        if(f[mask] == 0) continue;
+        ll w = p2[f[mask]] - 1;
+        int cntBit = __builtin_popcount(mask);
+        if (cntBit % 2 == 1) res = (res - w + MOD) % MOD;
+        else res = (res + w) % MOD;
+    }
+    cout<<res;
+    KILL();
+}
