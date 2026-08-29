83eecb994f461f2fe2dd60ea05d9426bcaf98d9c
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:40:29 +0700

    Add Hamiltonian Wall [C] from Codeforces

---
+++ 1766/C - Hamiltonian Wall/solution.cpp
@@ -0,0 +1,138 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+#define traillingzero(x) __builtin_ctzll(x)
+#define cntbit1(x) __builtin_popcountll(x)
+#define leadingzero(x) __builtin_clzll(x)
+#define TDN signed main()
+const int INF = 1e9; const int MOD = 1e9+7;
+int getbit(int x, int k){return (x >> k) & 1;}
+int offbit(int x, int k){return x & (~(1 << k));}
+ 
+void solve() {
+    int n;
+    cin >> n;
+ 
+    string s[2];
+    cin >> s[0] >> s[1];
+ 
+    for (int t = 0; t < 2; t++) {
+        int x = t;
+        int ok = 1;
+        for (int i = 0; i < n; i++) {
+            if (s[x][i] != 'B') ok = 0;
+            if (s[!x][i] == 'B') x ^= 1;
+        }
+ 
+        if (ok) {
+            cout << "YES
+";
+            return;
+        }
+    }
+ 
+    cout << "NO
+";
+}
+ 
+TDN
+{
+    fast;
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    auto start = clock();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    cerr << endl << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << ' ' << 'm' << 's' << endl;
+}
