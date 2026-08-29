9aceb0031e1f212c9e6fb82c241ae9ef98e828b8
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:33:54 +0700

    Add Raising Bacteria [A] from Codeforces

---
+++ 579/A - Raising Bacteria/solution.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define EYMK signed main()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fi first
+#define se second
+#define each(x) for(auto & i : x)
+#define vvi vector<vi>
+#define pb push_back
+#define endl '
+'
+#define yesno(x) cout << ((x) ? "YES
+" : "NO
+")
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+ 
+const int MOD = 1e9 + 7;
+const int LARGEPRIME = 998244353;
+const int INF_INT = 2147483647;
+const ll INF_LL = 2 * 1e18;
+const int dx[] = {-1, 1, 0, 0};
+#define start_time auto start = clock();
+#define cerr_time cerr <<endl<< "Time: " << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms
+";
+const int dy[] = {0, 0, -1, 1};
+const int dx8[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int n;
+int solve(int a=n,int cnt=1){
+    int b=1;
+    while(b<a){
+        b*=2;
+        if(b>a){
+            b/=2;
+            break;
+        }
+    }
+    if(b==a)return cnt;
+    return solve(a-b,cnt+1);
+}
+ 
+EYMK
+{
+    fast;
+ 
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    cin>>n;
+    cout<<solve(n);
+    return 0;
+}
+ 
