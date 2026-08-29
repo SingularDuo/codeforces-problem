5b54472aef12acd12b0851ea7c5f44e9f2345df9
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:37:53 +0700

    Add Diluc and Kaeya [C] from Codeforces

---
+++ 1536/C - Diluc and Kaeya/solution.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+ 
+const int MOD = 1e9 + 7;
+const int MOD2 = 998244353;
+const int N = 1e5;
+const int MAXA = 1e9;
+const int INF = 1e9; // change to 1e18 if using long long 
+ 
+int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return abs(a); 
+}
+void solve()
+{
+    int n; 
+    string s;
+    cin>>n>>s;
+    map<pii, int> mp;
+    int dcnt = 0, kcnt = 0;
+    for(char c : s)
+    {
+        if(c == 'D') dcnt++;
+        else kcnt++;
+        
+        int g = gcd(dcnt, kcnt);
+        mp[{dcnt / g, kcnt / g}]++;
+        cout<<mp[{dcnt / g, kcnt / g}]<<" ";
+    }
+    cout<<endl;
+}
+ 
+signed main()
+{
+    fast;
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
+// TDN LOVER
