0ac7f7cb430a9a2ae9e38e93c690aabb1328668d
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:27:18 +0700

    Add Permutation Warm-Up [A] from Codeforces

---
+++ 2108/A - Permutation Warm-Up/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+#define KING_PHAT signed main()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '
+'
+#define vec(type, a, size, value) vector<type> a(size, value);
+#define all(x) x.begin(), x.end()
+#define I_O(input_name, output_name) freopen((string(input_name) + ".inp").c_str(), "r", stdin); freopen((string(output_name) + ".out").c_str(), "w", stdout); fast;
+#define min(a,b) min((ll)a, (ll)b)
+#define max(a,b) max((ll)a, (ll)b)
+bool multitest = false;
+void init()
+{
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+    fast;
+}
+void sol()
+{
+    int n;
+    cin>>n;
+    long long result = 1 + (n * n / 2) / 2;
+    cout<<result<<endl;
+}
+KING_PHAT
+{
+    multitest = true;
+    // set state for multitest
+    // multitest = true;
+    //set state for I/O style
+    //I_O("test1", "test1");
+    fast;
+    if (multitest)
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            sol();
+        }
+    }
+    else
+    {
+        sol();
+    }
+    return 0;
+}
