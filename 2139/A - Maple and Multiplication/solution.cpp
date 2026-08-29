92cb40e6f3cff2df2ea7d3a8cb49e1d4e3cbf471
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:32:42 +0700

    Add Maple and Multiplication [A] from Codeforces

---
+++ 2139/A - Maple and Multiplication/solution.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define EYMK signed main()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fi first
+#define se second
+#define each(x) for(auto & i : x)
+#define vvi vector<vi>
+#define pb push_back
+#define endl '
+'
+#define yesno(x) cout << ((x) ? "YES
+" : "NO
+")
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+ 
+const int MOD = 1e9 + 7;
+const int LARGEPRIME = 998244353;
+const int INF_INT = 2147483647;
+const ll INF_LL = 2 * 1e18;
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+const int dx8[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 0, -1, 1, -1, 1, -1, 1};
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+void solve()
+{
+    int a, b; cin>>a>>b;
+    if(a == b)
+    {
+        cout<<0<<endl;
+        return;
+    }
+    if(max(a, b) % min(a, b) == 0)
+    {
+        cout<<1<<endl;
+        return;
+    }
+    else cout<<2<<endl;
+}
+ 
+EYMK
+{
+    fast;
+ 
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    int tc; cin>>tc;
+    while(tc--) solve();
+    return 0;
+}
