f0429733f4062aa9891d59d169ce9c88a69f60bf
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:24:59 +0700

    Add Cloudberry Jam [A] from Codeforces

---
+++ 2086/A - Cloudberry Jam/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t, n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        long long res = n*2;
+        cout << res << '
+';
+    }
+}
