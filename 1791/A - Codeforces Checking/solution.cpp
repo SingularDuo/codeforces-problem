01c1c327bbffc4b7be64f5bbaa35e7e4cc02b3e5
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:26:55 +0700

    Add Codeforces Checking [A] from Codeforces

---
+++ 1791/A - Codeforces Checking/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+#define KING_PHAT signed main()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '
+'
+#define vec(type, a, size, value) vector<type> a(size, value);
+#define all(x) x.begin(), x.end()
+#define I_O(input_name, output_name) freopen((string(input_name) + ".inp").c_str(), "r", stdin); freopen((string(output_name) + ".out").c_str(), "w", stdout); fast;
+bool multitest = false;
+void init()
+{
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+    fast;
+}
+void sol()
+{
+    char a;
+    cin>>a;
+    if(a == 'C' || a == 'o'||a=='d'||a=='e'||a=='f'||a=='o'||a=='r'||a=='c'||a=='e'||a=='s')cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+KING_PHAT
+{
+    // set state for multitest
+     multitest = true;
+    //set state for I/O style
+    //I_O("test1", "test1");
+    fast;
+    if (multitest)
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            sol();
+        }
+    }
+    else
+    {
+        sol();
+    }
+    return 0;
+}
