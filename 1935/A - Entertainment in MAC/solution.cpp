0c19bb2686285959d244010783bfee8e632220fb
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:40:24 +0700

    Add Entertainment in MAC [A] from Codeforces

---
+++ 1935/A - Entertainment in MAC/solution.cpp
@@ -0,0 +1,161 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+#define traillingzero(x) __builtin_ctzll(x)
+#define cntbit1(x) __builtin_popcountll(x)
+#define leadingzero(x) __builtin_clzll(x)
+#define TDN signed main()
+const int INF = 1e9; const int MOD = 1e9+7;
+int getbit(int x, int k){return (x >> k) & 1;}
+int offbit(int x, int k){return x & (~(1 << k));}
+ 
+bool cmp(string s, string t)
+{
+    if(s.size() != t.size()) return s.size() < t.size();
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == t[i]) continue;
+        if(s[i] < t[i]) return true;
+        else return false;
+    }
+    return true;
+}
+void solve()
+{
+    int n; cin>>n;
+    string s; cin>>s;
+    string t = s;
+    reverse(all(t));
+    if(t == s)
+    {
+        cout<<t<<endl;
+        return;
+    }
+    if(cmp(t, s))
+    {
+        if(n % 2 == 1)
+        {
+            cout<<t<<endl;
+            return;
+        }
+        else
+        {
+            t += s;
+            cout<<t<<endl;
+            return;
+        }
+    }
+    else
+    {
+        if(n % 2 == 0)
+        {
+            cout<<s<<endl;
+            return;
+        }
+        else
+        {
+            s += t;
+            reverse(all(s));
+            cout<<s<<endl;
+            return;
+        }
+    }
+}
+ 
+TDN
+{
+    fast;
+    auto start = clock();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    cerr << endl << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << ' ' << 'm' << 's' << endl;
+}
