ca15fe86c74cc0c382fdfe0f5a815cf619c7f568
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:27:55 +0700

    Add Adding Digits [A] from Codeforces

---
+++ 260/A - Adding Digits/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+#define KING_PHAT signed main()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '
+'
+#define vec(type, a, size, value) vector<type> a(size, value);
+#define all(x) x.begin(), x.end()
+#define I_O(input_name, output_name) freopen((string(input_name) + ".inp").c_str(), "r", stdin); freopen((string(output_name) + ".out").c_str(), "w", stdout); fast;
+#define min(a,b) min((ll)a, (ll)b)
+#define max(a,b) max((ll)a, (ll)b)
+bool multitest = false;
+void init()
+{
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+    fast;
+}
+void sol()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    ll ans = a*10;
+    for(int i = 0; i <= 9; i++)
+    {
+        int current = ans + i;
+        if(current % b == 0)
+        {
+            int zero = c--;
+            cout<<current;
+            for(int j = 1; j <= zero-1; j++)cout<<0;
+            return;
+        }
+        
+    }
+    cout<<-1;
+}
+KING_PHAT
+{
+    // set state for multitest
+    // multitest = true;
+    //set state for I/O style
+    //I_O("test1", "test1");
+    fast;
+    if (multitest)
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            sol();
+        }
+    }
+    else
+    {
+        sol();
+    }
+    return 0;
+}
