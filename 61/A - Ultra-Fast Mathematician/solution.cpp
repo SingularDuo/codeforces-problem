059716b149c40e294ef405f9a6d4f028dbb7c7a8
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:28:42 +0700

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,57 @@
+/*
+        _.-- ,.--.
+      .'   .'     /
+       @       |'..--------._
+     /      \._/              '.
+    /  .-.-                     \
+   (  /    \                     \
+   \      '.                  | #
+    \       \   -.           /
+     :\       |    )._____.'   \
+      "       |   /  \  |  \    )
+    Kduckp       |   |./'  :__ \.-'
+              '--'
+*/
+/*
+ * Author: Kduckp
+ * Generated on: 2025-05-16 12:19:13
+*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define endl "
+"
+#define min(a, b) min((ll)a, (ll)b)
+#define max(a, b) max((ll)a, (ll)b)
+#define fi first
+#define se second
+#define si size()
+#define all(x) x.begin(), x.end()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ull unsigned long long
+#define pb push_back
+#define __TOISETHIVOI__ signed main()
+ 
+const int MOD = 1e9 + 7;
+const int INF = 2 * 1e18;
+ 
+void init() {
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+    fast;
+}
+ 
+__TOISETHIVOI__ {
+    //init();
+    string s, s1, res = "";
+    cin>>s>>s1;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] != s1[i])res += '1';
+        else res += '0';
+    }
+    cout<<res;
+    return 0;
+}
