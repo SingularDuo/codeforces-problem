95ddbc6fe812c8aa7c41f45613a1f414a46acc42
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:47:21 +0700

    Add Tetrahedron [E] from Codeforces

---
+++ 166/E - Tetrahedron/solution.cpp
@@ -0,0 +1,138 @@
+/*
+Author: duxp
+"IT WORKED ON MY MACHINE!"
+"My code is correct, the judge is just having a bad day."
+"My code is perfect, that must be compiler's fault"
+*/
+#include <bits/stdc++.h>
+ 
+#ifndef ONLINE_JUDGE
+    #ifdef __APPLE__
+        #include <mach/mach.h>
+    #endif
+#endif
+ 
+using namespace std;
+ 
+#define ll long long
+#define int ll
+#define ld long double 
+#define pb push_back
+#define all(x) x.begin() + 1, x.end()
+#define nall(x) x.begin(), x.end()
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define vpii vector<pii>
+#define endl '
+'
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, x) memset(a, x, sizeof(a))
+ 
+#define _______TOISECOGIAIVOI_______ signed main()
+#define KILL() exit(0)
+#define NAME "duxp"
+#ifndef ONLINE_JUDGE
+    #ifdef __APPLE__
+        #include <mach/mach.h>
+        #include <mach-o/getsect.h>
+        #include <mach-o/dyld.h>
+        extern "C" struct mach_header_64 _mh_execute_header;
+    #endif
+    void force_memory_allocation() {
+        #ifdef __APPLE__
+            unsigned long size;
+            const char* sections[] = {"__bss", "__common"};
+            
+            for (const char* sec : sections) {
+                uint8_t* ptr = getsectiondata(&_mh_execute_header, "__DATA", sec, &size);
+                if (ptr) {
+                    for (unsigned long i = 0; i < size; i += 4096) {
+                        *(volatile char*)(ptr + i) = *(ptr + i);
+                    }
+                }
+            }
+        #endif
+    }
+    void log_resource() {
+        double mem_mb = 0;
+        #ifdef __APPLE__
+            struct task_basic_info t_info;
+            mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+            if (task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count) == KERN_SUCCESS) {
+                mem_mb = t_info.resident_size / (1024.0 * 1024.0);
+            }
+            mem_mb -= 1.5; 
+            if (mem_mb < 0) mem_mb = 0;
+        #else
+            struct rusage usage;
+            if (getrusage(RUSAGE_SELF, &usage) == 0) {
+                mem_mb = usage.ru_maxrss / 1024.0;
+            }
+        #endif
+ 
+        cerr << fixed << setprecision(3) << "
+========== DEBUG ==========
+";
+        cerr << "Time: " << 1000.0 * clock() / CLOCKS_PER_SEC << " ms
+";
+        cerr << "Memory (User): " << mem_mb << " MB
+";
+        cerr << "===========================
+";
+    }
+#else
+    void force_memory_allocation() {}
+    void log_resource() {}
+#endif
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+template <typename T>
+inline int getbit(T x, int k) { return (x >> k) & 1; }
+template <typename T>
+inline T onbit(T x, int k) { return x | (T(1) << k); }
+template <typename T>
+inline T offbit(T x, int k) { return x & ~(T(1) << k); }
+ 
+const ll OO = 1e18;
+const int oo = 1e9;
+const int MOD = 1000000007;
+const int MAXN = 200000;
+ 
+int n;
+ 
+                            _______TOISECOGIAIVOI_______
+{
+    fast;
+    if (fopen(NAME ".inp", "r")) {
+        freopen(NAME ".inp", "r", stdin);
+        freopen(NAME ".out", "w", stdout);
+    }
+    force_memory_allocation();
+ 
+    cin>>n;
+    int zD = 1;
+    int zABC = 0;
+    for (int i = 1; i <= n; i++) {
+        int nzD = zABC * 3LL % MOD;
+        int nzABC = (zABC * 2LL + zD) % MOD;
+        zD = nzD;
+        zABC = nzABC;
+    }
+    cout << zD;
+ 
+    log_resource();
+    KILL();
+}
