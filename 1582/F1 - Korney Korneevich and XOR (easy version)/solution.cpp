0ce9e363a4d220a017304beb28128ec7610c0f5b
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:45:48 +0700

    Add Korney Korneevich and XOR (easy version) [F1] from Codeforces

---
+++ 1582/F1 - Korney Korneevich and XOR (easy version)/solution.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+#define all(x) x.begin() + 1, x.end()
+#define nall(x) x.begin(), x.end()
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, x) memset(a, x, sizeof(a))
+ 
+#define trailingzero(x) __builtin_ctzll(x)
+#define cntbit1(x) __builtin_popcountll(x)
+#define leadingzero(x) __builtin_clzll(x)
+ 
+#define _______TOISETHIVOI_______ signed main()
+#define KILL() exit(0)
+#define NAME "duxp"
+ 
+template <typename T>
+inline int getbit(T x, int k) { return (x >> k) & 1; }
+template <typename T>
+inline T onbit(T x, int k) { return x | (T(1) << k); }
+template <typename T>
+inline T offbit(T x, int k) { return x & ~(T(1) << k); }
+ 
+const ll OO = 1e18;
+const int oo = 1e9;
+const int MOD = 1000000007;
+const int MOD2 = 998244353;
+const int MAXN = 200000;
+ 
+int n;
+int a[MAXN + 5];
+ 
+_______TOISETHIVOI_______
+{
+    fast;
+    if (FILE *f = fopen(NAME ".inp", "r")) {
+        fclose(f);
+        freopen(NAME ".inp", "r", stdin);
+        freopen(NAME ".out", "w", stdout);
+    }
+ 
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+ 
+    const int MAXX = 512;
+    static int mini[MAXX];
+    for (int x = 0; x < MAXX; x++) mini[x] = oo;
+ 
+    // subsequence rỗng: XOR = 0, giá trị kết thúc = -1
+    mini[0] = -1;
+ 
+    for (int i = 1; i <= n; i++) {
+        static int old[MAXX];
+        for (int x = 0; x < MAXX; x++) old[x] = mini[x];
+ 
+        for (int x = 0; x < MAXX; x++) {
+            if (old[x] < a[i]) {
+                int nx = x ^ a[i];
+                mini[nx] = min(mini[nx], a[i]);
+            }
+        }
+    }
+ 
+    vector<int> res;
+    for (int x = 0; x < MAXX; x++) {
+        if (mini[x] < oo) res.pb(x);
+    }
+ 
+    cout << res.size() << '
+';
+    for (int x : res) cout << x << ' ';
+ 
+    KILL();
+}
