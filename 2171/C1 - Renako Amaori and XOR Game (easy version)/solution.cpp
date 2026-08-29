0b6ed0d257d247088403b3e8ba35e9169d083f40
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:40:44 +0700

    Add Renako Amaori and XOR Game (easy version) [C1] from Codeforces

---
+++ 2171/C1 - Renako Amaori and XOR Game (easy version)/solution.cpp
@@ -0,0 +1,146 @@
+/*
+                _,.---.---.---.--.._ 
+            _.-' `--.`---.`---'-. _,`--.._
+           /`--._ .'.     `.     `,`-.`-._\
+          ||   \  `.`---.__`__..-`. ,'`-._/
+     _  ,`\ `-._\   \    `.    `_.-`-._,``-.
+  ,`   `-_ \/ `-.`--.\    _\_.-'\__.-`-.`-._`.
+ (_.o> ,--. `._/'--.-`,--`  \_.-'       \`-._ \
+  `---'    `._ `---._/__,----`           `-. `-\
+            /_, ,  _..-'                    `-._\
+            \_, \/ ._(
+             \_, \/ ._\
+              `._,\/ ._\
+                `._// ./`-._
+                  `-._-_-_.-'
+      Author: Kieu Duc Phat
+ 
+*/
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+ 
+using namespace std;
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define int long long
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+#define traillingzero(x) __builtin_ctzll(x)
+#define cntbit1(x) __builtin_popcountll(x)
+#define leadingzero(x) __builtin_clzll(x)
+#define TDN signed main()
+int getbit(int x, int k){return (x >> k) & 1;}
+int offbit(int x, int k){return x & (~(1 << k));}
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int MOD2 = 998244353;
+ 
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1), b(n + 1);
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    for(int i = 1; i <= n; i++) cin >> b[i];
+    int tot = 0, ps = 0;
+    for(int i =1; i <= n; i++)
+    {
+      tot ^= (a[i] ^ b[i]);
+      if((a[i] ^ b[i])) ps = i;
+    }
+    if(tot == 0) cout<<"TIE"<<endl;
+    else if(ps % 2 == 1) cout<<"ajisai"<<endl;
+    else cout<<"mai"<<endl;
+}
+ 
+ 
+TDN
+{
+    fast;
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    int tc; cin>>tc;
+    while(tc--) solve();
+}
+ 
