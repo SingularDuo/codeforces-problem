27e08e2e39a9904a4baa4cfbe34c050f26960dc9
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:24:40 +0700

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "
+"
+#define int long long
+signed main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+    return 0;
+}
