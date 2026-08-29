f13a103fae33d4548a7027de6ca91591a6f4d54d
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:33:10 +0700

    Add Price Tags [D] from Codeforces

---
+++ 2144/D - Price Tags/solution.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define EYMK signed main()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fi first
+#define se second
+#define each(x) for(auto & i : x)
+#define vvi vector<vi>
+#define pb push_back
+#define endl '
+'
+#define yesno(x) cout << ((x) ? "YES
+" : "NO
+")
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+const int MOD = 1e9 + 7;
+const int LARGEPRIME = 998244353;
+const int INF_INT = 2147483647;
+const ll INF_LL = 2 * 1e18;
+const int dx[] = {-1, 1, 0, 0};
+#define start_time auto start = clock();
+#define cerr_time cerr <<endl<< "Time: " << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms
+";
+const int dy[] = {0, 0, -1, 1};
+const int dx8[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 0, -1, 1, -1, 1, -1, 1};
+ 
+void solve() {
+    int n;
+    ll y;
+    cin >> n >> y;
+ 
+    vector<int> c(n);
+    int max_c = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> c[i];
+        max_c = max(max_c, c[i]);
+    }
+ 
+    vector<int> freq(max_c + 2, 0);
+    for (int val : c) freq[val]++;
+ 
+    vector<int> f(max_c + 2, 0);
+    for (int i = 1; i <= max_c + 1; ++i)
+        f[i] = f[i - 1] + freq[i];
+ 
+    ll res = LLONG_MIN;
+ 
+    for (int x = 2; x <= max_c + 1; ++x) {
+        ll ttv = 0;
+        ll reu = 0;
+ 
+        for (ll price = x; price <= max_c + x; price += x) {
+            ll left = price - x;
+            ll right = min((ll)max_c + 1, price);
+ 
+            ll cnt = f[right] - f[left];
+            if (cnt == 0) continue;
+ 
+            ll tag_price = price / x;
+            ttv += cnt * tag_price;
+ 
+            if (tag_price <= max_c)
+                reu += min(cnt, (ll)freq[tag_price]);
+        }
+ 
+        ll need_to_print = n - reu;
+        ll income = ttv - need_to_print * y;
+        res = max(res, income);
+    }
+ 
+    cout << res << '
+';
+}
+EYMK
+{
+    fast;
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+ 
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
