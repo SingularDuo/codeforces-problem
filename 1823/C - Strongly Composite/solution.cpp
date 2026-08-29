6fb8c7a94fada7e9cd9f54dc02f996e9502fae84
Kduckp <ducphatkieu@gmail.com>
2026-08-29 17:34:03 +0700

    Add Strongly Composite [C] from Codeforces

---
+++ 1823/C - Strongly Composite/solution.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define EYMK signed main()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fi first
+#define se second
+#define each(x) for(auto & i : x)
+#define vvi vector<vi>
+#define pb push_back
+#define endl '
+'
+#define yesno(x) cout << ((x) ? "YES
+" : "NO
+")
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define debug3(x, y, z) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+ 
+const int MOD = 1e9 + 7;
+const int LARGEPRIME = 998244353;
+const int INF_INT = 2147483647;
+const ll INF_LL = 2 * 1e18;
+const int dx[] = {-1, 1, 0, 0};
+#define start_time auto start = clock();
+#define cerr_time cerr <<endl<< "Time: " << (clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms
+";
+const int dy[] = {0, 0, -1, 1};
+const int dx8[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 0, -1, 1, -1, 1, -1, 1};
+ 
+void solve()
+{
+    int n; cin>>n;
+    map<int, int> mp;
+    for(int i = 1; i <= n; i++)
+    {
+        int x; cin>>x;
+        for (int j = 2; j * j <= x; j++) {
+            while (x % j == 0) {
+                x /= j;
+                mp[j]++;
+            }
+        }
+        if (x != 1) {
+            mp[x]++;
+        }
+    }
+    int res = 0, rem = 0;
+    for (pair <int, int> p : mp) {
+        int num = p.first;
+        int cnt = p.second;
+        res += cnt / 2;
+        rem += cnt % 2;
+    }
+ 
+    res += rem / 3;
+    cout<<res<<endl;
+}
+ 
+EYMK
+{
+    fast;
+ 
+    if (fopen("task.inp", "r")) {
+        freopen("task.inp", "r", stdin);
+        freopen("task.out", "w", stdout);
+    }
+    start_time
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+    cerr_time
+    return 0;
+}
